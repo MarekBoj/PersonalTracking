43af5d90cecb2d9e8fb81835e3daaf56d6fed25d
marekbojarski <marek.bojarski@microsoft.wsei.edu.pl>
2022-07-10 00:51:18 +0200

    Added updating, adding positions, and list position page

---
+++ PersonalTracking/PersonalTracking/PersonalTracking/Pages/PositionPage.xaml.cs
@@ -0,0 +1,83 @@
+﻿using PersonalTracking.DB;
+using PersonalTracking.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PersonalTracking.Pages
+{
+    /// <summary>
+    /// Interaction logic for PositionPage.xaml
+    /// </summary>
+    public partial class PositionPage : Window
+    {
+        public PositionPage()
+        {
+            InitializeComponent();
+        }
+
+        PERSONELTRACKINGContext db = new PERSONELTRACKINGContext();
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            var list = db.Departments.ToList();
+            cmbDepartment.ItemsSource = list;
+            cmbDepartment.DisplayMemberPath = "DepartmentName";
+            cmbDepartment.SelectedValuePath = "Id";
+            cmbDepartment.SelectedIndex = -1;
+            if (model != null && model.Id != 0)
+            {
+                cmbDepartment.SelectedValue = model.DepartmentId;
+                txtPositionName.Text = model.PositionName;
+            }
+        }
+
+        public PositionModel model;
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            if (cmbDepartment.SelectedIndex == -1 || txtPositionName.Text.Trim()=="")
+            {
+                MessageBox.Show("Please fill all areas");
+            }
+            else
+            {
+                if (model != null && model.Id != 0)
+                {
+                    Position pst = new Position();
+                    pst.DepartmentId = (int)cmbDepartment.SelectedValue;
+                    pst.Id = model.Id;
+                    pst.PositionName = txtPositionName.Text;
+                    db.Positions.Update(pst);
+                    db.SaveChanges();
+                    MessageBox.Show("Position was Updated!");
+                }
+                else
+                {
+
+                    Position position = new Position();
+                    position.PositionName = txtPositionName.Text;
+                    position.DepartmentId = Convert.ToInt32(cmbDepartment.SelectedValue);
+                    db.Positions.Add(position);
+                    db.SaveChanges();
+                    cmbDepartment.SelectedIndex = -1;
+                    txtPositionName.Clear();
+                    MessageBox.Show("Position was added!");
+                }
+            }
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+
+        }
+    }
+}
---
+++ PersonalTracking/PersonalTracking/PersonalTracking/Views/PositionList.xaml.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PersonalTracking.DB;
+using PersonalTracking.Models;
+using PersonalTracking.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PersonalTracking.Views
+{
+    /// <summary>
+    /// Interaction logic for PositionList.xaml
+    /// </summary>
+    public partial class PositionList : UserControl
+    {
+        public PositionList()
+        {
+            InitializeComponent();
+        }
+        PERSONELTRACKINGContext db = new PERSONELTRACKINGContext();
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            FillGrid();
+        }
+        
+        void FillGrid()
+        {
+            var list = db.Positions.Include(X => X.Department).Select(a => new
+            {
+                positionId = a.Id,
+                PositionName = a.PositionName,
+                departmentId = a.DepartmentId,
+                departmentName = a.Department.DepartmentName
+            }).OrderBy(x => x.PositionName).ToList();
+            List<PositionModel> modellist = new List<PositionModel>();
+            foreach (var item in list)
+            {
+                PositionModel model = new PositionModel();
+                model.Id = item.positionId;
+                model.PositionName = item.PositionName;
+                model.DepartmentName = item.departmentName;
+                model.DepartmentId = item.departmentId;
+                modellist.Add(model);
+
+            }
+            gridPosition.ItemsSource = modellist;
+        }
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            PositionPage page = new PositionPage();
+            page.ShowDialog();
+            FillGrid();
+        }
+
+        private void btnUpdate_Click(object sender, RoutedEventArgs e)
+        {
+            PositionModel model = (PositionModel)gridPosition.SelectedItem;
+            if(model != null && model.Id != 0)
+            {
+                PositionPage page = new PositionPage();
+                page.model = model;
+                page.ShowDialog();
+                FillGrid();
+            }
+
+        }
+    }
+}
